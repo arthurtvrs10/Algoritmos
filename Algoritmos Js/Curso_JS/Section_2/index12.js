3daa8b90cb5dd5449027c54350581ea92a4de77e
arthurtvrs10 <arthurtavares0222@gmail.com>
2026-01-08 22:12:31 -0300

    Section 2: aula 12 ´variáveis com let´

---
+++ Algoritmos Js/Curso_JS/Section_2/index12.js
@@ -0,0 +1,28 @@
+/*let name = 'João';
+var nome = "flavio";
+
+console.log(name ,'nasceu em 1984');
+console.log("Em 2000 " + name + " conheceu " + nome + ".");
+console.log('João casou-se com ' + nome + ' em 2012.');
+console.log(nome, " teve um filho com", nome , " em 2015.");
+console.log('O filho de ',nome,' se chama Eduardo.');
+*/
+
+ /*
+let nome; // só declara a variável
+nome = "João"; // Inicializando a variável
+console.log(nome);
+nome = "Flávio"; // Reatribuindo valor
+console.log(nome);
+*/
+
+/*
+*   Não pode declarar a variável mais de uma vez com let, mas pode reatribuir o valor.
+*   ex: let nome = "João";
+        let nome = "Flávio";  <--- Isso dará erro.
+*
+*/
+
+let nomeCliente = 'Luiz';
+let nomecliente = 'Otávio';
+console.log(nomeCliente, nomecliente);
